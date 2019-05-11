5c412b56d4edd4e93fe9265cde3f5fe1dc5dd239
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-11 12:55:26 +0100

    destructed name, email and id of object

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
+
     return (
-        <div>
-            <img src="" alt="robots"/>
+    
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
+
     );
 }
 
