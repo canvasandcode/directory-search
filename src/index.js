d8ff8081bcbb338561b60149b9e9858ff3886bcb
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-11 12:20:20 +0100

    removed course files

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'} />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
