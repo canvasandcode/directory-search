0352c45e6c4b263011863cf0094ac622c1dd5b6a
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-11 12:07:44 +0100

    tachyons added

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hello.css';
+
+function Hello() {
+    return(
+        <div className="f1 tc">
+            <h1>Hello World</h1>
+            <p>Welcome to React</p>
+        </div> 
+    );
+    
+}
+  
+export default Hello;
